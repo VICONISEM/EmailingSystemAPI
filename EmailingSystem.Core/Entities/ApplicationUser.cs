62edce280503e551c16ab19715f98e5ca1494bc7
victor nisem <105614421+VICONISEM@users.noreply.github.com>
2024-12-07 00:52:51 +0200

    CreateAllEntities

---
+++ EmailingSystem.Core/Entities/ApplicationUser.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailingSystem.Core.Entities
+{
+    public class ApplicationUser : IdentityUser<int>
+    {
+        public string Name { get; set; } = null!;
+
+
+        private string NormalizedNameAttribute = null!;
+
+        public string NormalizedName
+        {
+            get { return NormalizedNameAttribute; }
+            set { NormalizedNameAttribute = Name.Trim().ToUpper();}
+        }
+
+        public string NationalId { get; set; } = null!;
+        public string? PicturePath { get; set; } = null!;
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; } = null!;
+        public int SignatureId { get; set; }
+        public Signature Signature { get; set; } = null!;
+    }
+}
---
+++ EmailingSystem.Core/Entities/UserInbox.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailingSystem.Core.Entities
+{
+    public class UserInbox
+    {
+        public int UserId { get; set; }
+        public ApplicationUser User { get; set; } = null!;
+        public long ConversationId { get; set; }
+        public Conversation Conversation { get; set; } = null!;
+
+    }
+}
