6a714bd330b7347c20a93ea2cfc299dfb5efbd47
victor nisem <105614421+VICONISEM@users.noreply.github.com>
2024-12-10 23:15:30 +0200

    BuildSpecifecationPattren

--- EmailingSystemAPI/Controllers/ConversationsController.cs
+++ EmailingSystemAPI/Controllers/ConversationsController.cs
@@ -1,6 +1,8 @@
 ﻿using EmailingSystem.Core.Contracts.Repository.Contracts;
+using EmailingSystem.Core.Contracts.Specification.Contract;
 using EmailingSystem.Core.Entities;
 using EmailingSystem.Repository;
+using EmailingSystem.Repository.Data.Contexts;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -12,10 +14,12 @@ namespace EmailingSystemAPI.Controllers
     public class ConversationsController : ControllerBase
     {
         private readonly IConversationRepository conversationRepository;
+        private readonly EmailDbContext context;
 
-        public ConversationsController(IConversationRepository conversationRepository)
+        public ConversationsController(IConversationRepository conversationRepository,EmailDbContext context)
         {
             this.conversationRepository = conversationRepository;
+            this.context = context;
         }
 
         int UserId = 1;
@@ -24,6 +28,16 @@ namespace EmailingSystemAPI.Controllers
         public async Task<ActionResult<IEnumerable<Conversation>>> AllConversations(string Type)
         {
             var Conversations = await conversationRepository.GetConversationsByTypeAsync(UserId, Type).ToListAsync();
+
+            return Conversations;
+        }
+
+        [HttpGet("{AllConversationsWithSpecs}")]
+        public async Task<ActionResult<IEnumerable<Conversation>>> AllConversationsWithSpecs(string Type)
+        {
+            var BaseSpec = new BaseSpecification<UserInbox>(x => x.UserId == UserId) {};
+            BaseSpec.Includes.Add(u => u.Conversation);
+            var Conversations = await SpecificationEvalutor<UserInbox>.GetQuery(context.Set<UserInbox>(),BaseSpec).Select(x=>x.Conversation).ToListAsync();
 
             return Conversations;
         }
