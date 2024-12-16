4b4c4adc18a04c98a7db4bb2abc3c20afd935510
victor nisem <105614421+VICONISEM@users.noreply.github.com>
2024-12-16 17:18:38 +0200

    HandelDraftFeature

---
+++ EmailingSystem.Core/Entities/DraftAttachments.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailingSystem.Core.Entities
+{
+    public class DraftAttachments
+    {
+        public int Id { get; set; }
+        public long DraftConversationId { get; set; }
+        public DraftConversations draftConversations { get; set; }
+        public string AttachmentPath { get; set; }
+
+    }
+}
