e9060b67202a1e6d022ea4a9aa3a51faa851e9b7
Mohamed Essam <Mohamed Essam@DESKTOP-SJSM9SF>
2024-12-16 20:28:50 +0200

    MappingProfile

--- EmailingSystemAPI/DTOs/UserDto.cs
+++ EmailingSystemAPI/DTOs/UserDto.cs
@@ -4,12 +4,10 @@
     {
         public string Email { get; set; } = null!;
         public string Name { get; set; } = null!;
-        public string? PictureUrl { get; set; }
-        public int? SignatureId { get; set; }
-        public string DepartmentName { get; set; } = null!;
-        public string NationalId { get; set; } = null!;
-        public string? AccessToken { get; set; }
-        public DateTime? RefreshTokenExpirationTime  { get; set; }
+        public string? Role { get; set; }
+        public string? PicturePath { get; set; } = null!;
+        public string? CollegeName { get; set; } = null!;
+        public string? DepartmentName { get; set; } = null!;
 
     }
 }
