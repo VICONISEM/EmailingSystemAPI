92e395837d0b4390b42075e27060da5c81cce92a
victor nisem <105614421+VICONISEM@users.noreply.github.com>
2024-12-17 20:07:14 +0200

    ApplaySpecificationOnDraft

---
+++ EmailingSystem.Core/Contracts/Specifications.Contracts/ConversationSpecs/PaginatedConversation/ConversationDraftSpecificationForCountPagination.cs
@@ -0,0 +1,21 @@
+﻿using EmailingSystem.Core.Contracts.Specification.Contract;
+using EmailingSystem.Core.Contracts.Specifications.Contracts.SpecsParams;
+using EmailingSystem.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailingSystem.Core.Contracts.Specifications.Contracts.ConversationSpecs.PaginatedConversation
+{
+    public class ConversationDraftSpecificationForCountPagination:BaseSpecification<DraftConversations>
+    {
+        public ConversationDraftSpecificationForCountPagination(ConversationSpecParams Specs , int UserId) 
+        {
+
+            Criteria = D => D.SenderId == UserId && (string.IsNullOrEmpty(Specs.Search) || D.Subject.Trim().ToLower().Contains(Specs.Search));
+
+        }
+    }
+}
