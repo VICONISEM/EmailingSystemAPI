9ccad20a2fe64f7ea4db5c7b05e0f1d06e22b99f
victor nisem <105614421+VICONISEM@users.noreply.github.com>
2024-12-17 23:11:33 +0200

    CollogeSpecification

---
+++ EmailingSystem.Core/Contracts/Specifications.Contracts/CollegeSpecs/CollegeSpecificationsGetAll.cs
@@ -0,0 +1,21 @@
+﻿using EmailingSystem.Core.Contracts.Specification.Contract;
+using EmailingSystem.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailingSystem.Core.Contracts.Specifications.Contracts.CollegeSpecs
+{
+    public class CollegeSpecificationsGetAll:BaseSpecification<College>
+    {
+        public CollegeSpecificationsGetAll(CollegeSpecsParams Specs) 
+        {
+            Criteria = C => (string.IsNullOrEmpty(Specs.Search) || (C.Name == Specs.Search || C.Abbreviation == Specs.Search));
+            AddInclude(C => C.Include(cc => cc.Departments));
+        
+        }
+    }
+}
