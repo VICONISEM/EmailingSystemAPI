975c274685251b7639d4d9fd8ac52136a07c61b7
victor nisem <105614421+VICONISEM@users.noreply.github.com>
2024-12-18 21:52:48 +0200

    MessageSpecification

---
+++ EmailingSystem.Core/Contracts/Specifications.Contracts/ConversationSpecs/MessagesInConversation/GetDraftMessageSpecification.cs
@@ -0,0 +1,22 @@
+﻿using EmailingSystem.Core.Contracts.Specification.Contract;
+using EmailingSystem.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailingSystem.Core.Contracts.Specifications.Contracts.ConversationSpecs.MessagesInConversation
+{
+    public class GetDraftMessageSpecification : BaseSpecification<Conversation>
+    {
+        public GetDraftMessageSpecification(int UserId,long ConversationId) 
+        {
+            Criteria = C => (C.Id == ConversationId);
+            AddInclude(In => In.Include(C => C.Messages.Where(M => M.SenderId == UserId && M.IsDraft)));
+
+
+        }
+    }
+}
