1c2b369081937574abe91fad8d770566a1b570ae
victor nisem <105614421+VICONISEM@users.noreply.github.com>
2024-12-18 03:25:36 +0200

    CollegeDto

---
+++ EmailingSystemAPI/DTOs/CollegeAddDto.cs
@@ -0,0 +1,8 @@
+﻿namespace EmailingSystemAPI.DTOs
+{
+    public class CollegeAddDto
+    {
+        public string? Name { get; set; }
+        public string? Abbreviation { get; set; }
+    }
+}
---
+++ EmailingSystemAPI/DTOs/CollegesDto.cs
@@ -0,0 +1,12 @@
+﻿namespace EmailingSystemAPI.DTOs
+{
+    public class CollegesDto
+    {
+        public int Id { get; set; }
+        public string? Name { get; set;}
+
+        public string? Abbreviation { get; set; }
+
+        public List<DepartmentDto>? Departments { get; set; }
+    }
+}
