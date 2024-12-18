452abc72a07381cae89de986160e938eedfda6ba
victor nisem <105614421+VICONISEM@users.noreply.github.com>
2024-12-18 03:25:17 +0200

    College Controller

--- EmailingSystemAPI/Controllers/CollegeController.cs
+++ EmailingSystemAPI/Controllers/CollegeController.cs
@@ -2,8 +2,13 @@
 using EmailingSystem.Core.Contracts;
 using EmailingSystem.Core.Contracts.Specifications.Contracts;
 using EmailingSystem.Core.Contracts.Specifications.Contracts.CollegeSpecs;
+using EmailingSystem.Core.Entities;
+using EmailingSystemAPI.DTOs;
+using EmailingSystemAPI.Helper;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EmailingSystemAPI.Controllers
 {
@@ -12,17 +17,73 @@ namespace EmailingSystemAPI.Controllers
     public class CollegeController : ControllerBase
     {
        private readonly IUnitOfWork unitOfWork;
+        private readonly IMapper mapper;
 
-        public CollegeController(IUnitOfWork unitOfWork)
+        public CollegeController(IUnitOfWork unitOfWork,IMapper mapper)
         {
             this.unitOfWork = unitOfWork;
+            this.mapper = mapper;
         }
 
-        [HttpGet]
-        public async Task<IActionResult<>> GetAllCollegesWithSpecsAsync(CollegeSpecsParams SpecsParams)
+        [HttpGet("GetAllColleges")]
+        public async Task<ActionResult<Pagination<CollegesDto>>> GetAllCollegesWithSpecsAsync([FromQuery]CollegeSpecsParams SpecsParams)
         {
-            var Colloges=
+            var Specs = new CollegeSpecificationsGetAll(SpecsParams);
+            var colleges=await unitOfWork.Repository<College>().GetAllQueryableWithSpecs(Specs).ToListAsync();
+            var Colleges = mapper.Map<List<CollegesDto>>(colleges);
+
+            var SpecsCount = new CollegeSpecificationsGetAllCount(SpecsParams);
+            var CollegesCount = await unitOfWork.Repository<College>().GetCountWithSpecs(SpecsCount);
+
+            return Ok(new Pagination<CollegesDto>(SpecsParams.PageIndex,SpecsParams.PageSize,CollegesCount,Colleges));
         }
+
+        [HttpPost("DeleteCollegeById")]
+        public async Task<ActionResult> DeleteCollegeById(int ?Id)
+        {
+            if (Id is null)
+            return BadRequest("there Is No Id");
+
+            var College = await unitOfWork.Repository<College>().GetByIdAsync(Id);
+            if (College is null)
+                return BadRequest(new { error = "there Is No College With This Id" });
+             unitOfWork.Repository<College>().Delete(College);
+            return Ok($"College Deleted Successfully");
+        }
+        [HttpPost("UpdateCollege")]
+        public async Task<ActionResult>UpdateCollege(CollegesDto college , int ?Id)
+        {
+            if(college is null || Id is null)
+            {
+                return BadRequest("There Is Error With Dto Or Id");
+            }
+            var College = await unitOfWork.Repository<College>().GetByIdAsync(Id);
+            if(College is null)
+            {
+                return BadRequest("There Is Error When Fetching College");
+            }
+
+            mapper.Map(college, College);
+
+            unitOfWork.Repository<College>().Update(College);
+            return Ok($"College Updated Successfully"); 
+
+        }
+
+        [HttpPost("AddCollege")]
+        public async Task<ActionResult<CollegeAddDto>>AddCollege(CollegeAddDto college) 
+        {
+            var Specs = new CollegeSpecificationCheckCollege(college.Name);
+            var IfExite = await unitOfWork.Repository<College>().GetAllQueryableWithSpecs(Specs).ToListAsync();
+            if(IfExite is null)
+            {
+                await unitOfWork.Repository<College>().AddAsync(mapper.Map<College>(college));
+                return Ok("College Added Successfully");
+            }
+            return BadRequest("The College Is Exsite already");
+        } 
+
+
 
     }
 }
