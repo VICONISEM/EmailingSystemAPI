42fe10014d0f8a7211c751fb38bbb59cb25e3251
victor nisem <105614421+VICONISEM@users.noreply.github.com>
2024-12-18 21:53:40 +0200

    EditeFileHandler

---
+++ EmailingSystem.Services/FileHandler.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailingSystem.Services
+{
+    public static class FileHandler
+    {
+        public static async Task<string> SaveFile(string FileKey,string FileFolder,IFormFile File)
+        {
+            string FileName = await FileNameConstructor(FileKey);
+            string FilePath=Path.Combine(Directory.GetCurrentDirectory(),"wwwroot",FileFolder,FileName);
+
+            using (var Fs=new FileStream(FilePath,FileMode.Create))
+            {
+                await File.CopyToAsync(Fs);
+            }
+            return Path.Combine(FileFolder, FileName);
+        }
+
+        public static async Task<bool> DeleteFile(string filePath)
+        {
+            string FilePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", filePath);
+            if (File.Exists(FilePath))
+            {
+
+                try
+                {
+                    string tempFilePath = Path.Combine(Path.GetDirectoryName(FilePath),$"{Guid.NewGuid()}_{Path.GetFileName(FilePath)}");
+                    File.Move(FilePath, tempFilePath);
+
+                    File.Delete(tempFilePath);
+                    return true;
+                }
+                catch (IOException ex)
+                {
+                    Console.WriteLine($"Error deleting File: {ex.Message}");
+                    return false;
+                }
+
+               
+            }
+            else { return false; }
+        }
+        private static async Task<string> FileNameConstructor(string FileKey)
+        => $"{FileKey}-{DateTime.Now.ToString("yyyyMMdd")}-{Guid.NewGuid()}";
+    }
+}
