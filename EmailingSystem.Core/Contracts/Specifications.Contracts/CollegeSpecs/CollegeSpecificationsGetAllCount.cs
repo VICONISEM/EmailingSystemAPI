3d72ded41a0b5a0a65e13d8b4dab9dd298b17154
victor nisem <105614421+VICONISEM@users.noreply.github.com>
2024-12-18 03:25:00 +0200

    CollegeSpecifications

--- EmailingSystem.Core/Contracts/Specifications.Contracts/CollegeSpecs/CollegeSpecificationsGetAllCount.cs
+++ EmailingSystem.Core/Contracts/Specifications.Contracts/CollegeSpecs/CollegeSpecificationsGetAllCount.cs
@@ -9,15 +9,12 @@ using System.Threading.Tasks;
 
 namespace EmailingSystem.Core.Contracts.Specifications.Contracts.CollegeSpecs
 {
-    public class CollegeSpecificationsGetAll:BaseSpecification<College>
+    public class CollegeSpecificationsGetAllCount:BaseSpecification<College>
     {
-        public CollegeSpecificationsGetAll(CollegeSpecsParams Specs) 
+        public CollegeSpecificationsGetAllCount(CollegeSpecsParams Specs) 
         {
             Criteria = C => (string.IsNullOrEmpty(Specs.Search) || (C.Name == Specs.Search || C.Abbreviation == Specs.Search));
             AddInclude(C => C.Include(cc => cc.Departments));
-
-            ApplyPagination(Specs.PageSize * (Specs.PageIndex - 1), Specs.PageSize);
-        
         }
     }
 }
