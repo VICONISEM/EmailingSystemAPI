89c06a87f4de7e0c1f63397c923684e12fdebcfa
victor nisem <105614421+VICONISEM@users.noreply.github.com>
2024-12-22 16:38:22 +0200

    Update Is Draft To False

--- EmailingSystem.Core/Entities/Message.cs
+++ EmailingSystem.Core/Entities/Message.cs
@@ -24,6 +24,6 @@ namespace EmailingSystem.Core.Entities
         public virtual ICollection<Attachment>? Attachments { get; set; }
         public bool SenderIsDeleted { get; set; }
         public bool ReceiverIsDeleted { get; set; }
-        public bool IsDraft { get; set; } = false;
+        public bool IsDraft {  get; set; } = false;
     }
 }
