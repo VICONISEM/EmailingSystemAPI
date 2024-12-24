ab27e73aa654d6abe6f146425d92d4d5632e1f72
Mohamed Essam <Mohamed Essam@DESKTOP-SJSM9SF>
2024-12-24 16:39:25 +0200

    DtosEdits

--- EmailingSystemAPI/DTOs/User/UserDto.cs
+++ EmailingSystemAPI/DTOs/User/UserDto.cs
@@ -5,7 +5,8 @@
         public string Email { get; set; } = null!;
         public string Name { get; set; } = null!;
         public string? Role { get; set; }
-        public string? PicturePath { get; set; } = null!;
+        public string? PictureURL { get; set; } = null!;
+        public string? SignatureURL { get; set; } = null!;
         public string? CollegeName { get; set; } = null!;
         public string? DepartmentName { get; set; } = null!;
         public string NationalId { get; set; } = null!;
