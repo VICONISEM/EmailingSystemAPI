d2ac64c0502f990d98400bb1113d64c23d020a97
Mohamed Essam <Mohamed Essam@DESKTOP-SJSM9SF>
2024-12-24 16:38:25 +0200

    ImageAndSignatureResolvers

---
+++ EmailingSystemAPI/Helper/UserProfileSignatureResolver.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using EmailingSystem.Core.Entities;
+using EmailingSystemAPI.DTOs.User;
+
+namespace EmailingSystemAPI.Helper
+{
+    public class UserProfileSignatureResolver : IValueResolver<ApplicationUser, UserDto, string>
+    {
+        private readonly IHttpContextAccessor configuration;
+
+        public UserProfileSignatureResolver(IHttpContextAccessor configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public string Resolve(ApplicationUser source, UserDto destination, string destMember, ResolutionContext context)
+        {
+            if (!string.IsNullOrEmpty(source.Signature?.FilePath))
+            {
+                var request = configuration?.HttpContext?.Request;
+                return $"{request?.Scheme}://{request?.Host}/{source.Signature.FilePath}";
+            }
+            else
+            {
+                return "Empty";
+            }
+        }
+
+    }
+}
