95804192cd34d4c70a84b694443b3e958cd2631c
victor nisem <105614421+VICONISEM@users.noreply.github.com>
2025-01-07 18:43:02 +0200

    AddResolverForprofileImage

---
+++ EmailingSystemAPI/Helper/ValidUserProfileResolver.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using EmailingSystem.Core.Entities;
+using EmailingSystemAPI.DTOs.User;
+
+namespace EmailingSystemAPI.Helper
+{
+    public class ValidUserProfileResolver : IValueResolver<ApplicationUser, AllowedUserDto, string>
+    {
+        private readonly IHttpContextAccessor configuration;
+
+        public ValidUserProfileResolver(IHttpContextAccessor configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public string Resolve(ApplicationUser source, AllowedUserDto destination, string destMember, ResolutionContext context)
+        {
+            if (!string.IsNullOrEmpty(source.PicturePath))
+            {
+                var request = configuration?.HttpContext?.Request;
+                return $"{request?.Scheme}://{request?.Host}/{source.PicturePath}";
+            }
+            else
+            {
+                return "Empty";
+            }
+        }
+    }
+}
