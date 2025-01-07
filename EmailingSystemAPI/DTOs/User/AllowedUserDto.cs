bdfdf6ddbbce9e44093023e5c0dcaab6a953a36a
victor nisem <105614421+VICONISEM@users.noreply.github.com>
2025-01-07 18:41:17 +0200

    AddDto

---
+++ EmailingSystemAPI/DTOs/User/AllowedUserDto.cs
@@ -0,0 +1,12 @@
+﻿namespace EmailingSystemAPI.DTOs.User
+{
+    public class AllowedUserDto
+    {
+        public int Id { get; set; }
+        public string Email { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public string? PictureURL { get; set; } = null!;
+        public string? CollegeName { get; set; } = null!;
+        public string? DepartmentName { get; set; } = null!;
+    }
+}
