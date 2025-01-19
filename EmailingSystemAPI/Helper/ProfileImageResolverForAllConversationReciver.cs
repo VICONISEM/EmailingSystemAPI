f5ac4a00be1dd20e321045c86dd30990c1b2c311
victor nisem <105614421+VICONISEM@users.noreply.github.com>
2025-01-19 22:29:16 +0200

    EditeMappingForImageOfSenderAndReciver

--- EmailingSystemAPI/Helper/ProfileImageResolverForAllConversationReciver.cs
+++ EmailingSystemAPI/Helper/ProfileImageResolverForAllConversationReciver.cs
@@ -5,33 +5,29 @@ using EmailingSystemAPI.DTOs.User;
 
 namespace EmailingSystemAPI.Helper
 {
-    public class ProfileImageResolverForAllConversation : IValueResolver<Conversation, ConversationDto, string>
+    public class ProfileImageResolverForAllConversationReciver : IValueResolver<Conversation, ConversationDto, string>
     {
 
         private readonly IHttpContextAccessor configuration;
 
-        public ProfileImageResolverForAllConversation(IHttpContextAccessor configuration)
+        public ProfileImageResolverForAllConversationReciver(IHttpContextAccessor configuration)
         {
             this.configuration = configuration;
         }
 
         public string Resolve(Conversation source, ConversationDto destination, string destMember, ResolutionContext context)
         {
-            if (!string.IsNullOrEmpty(source.Sender.PicturePath) && destination.SenderPictureURL is null)
-            {
-                var request = configuration?.HttpContext?.Request;
-                return $"{request?.Scheme}://{request?.Host}/{source.Sender.PicturePath}";
-            }
-            if (!string.IsNullOrEmpty(source.Receiver.PicturePath) && destination.SenderPictureURL is null)
+            if (source.Receiver.PicturePath is not null && destination.ReceiverPictureURL is null)
             {
                 var request = configuration?.HttpContext?.Request;
                 return $"{request?.Scheme}://{request?.Host}/{source.Receiver.PicturePath}";
             }
-
             else
             {
                 return "Empty";
             }
+
+
         }
 
     }
