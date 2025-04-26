d72c0ecc49eb3701bc26156fe7832dd5c27fe4d8
victor nisem <105614421+VICONISEM@users.noreply.github.com>
2025-04-26 17:21:44 +0300

    make Nullable  Fildes for files

--- EmailingSystemAPI/DTOs/User/UserToUpdateDto.cs
+++ EmailingSystemAPI/DTOs/User/UserToUpdateDto.cs
@@ -6,8 +6,8 @@ namespace EmailingSystemAPI.DTOs.User
     {
         public string Name { get; set; } = null!;
         public UserRole Role { get; set; }
-        public IFormFile Signature { get; set; }
-        public IFormFile Picture { get; set; }
+        public IFormFile? Signature { get; set; }
+        public IFormFile? Picture { get; set; }
         public string NationalId { get; set; } = null!;
         public int? CollegeId { get; set; } = null;
         public int? DepartmentId { get; set; } = null;
